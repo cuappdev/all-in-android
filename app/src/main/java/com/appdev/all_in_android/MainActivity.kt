658b99d8f2901d1f532793605a9ffebae0fdbf26
Andrew Cheung <andrewcheung360@gmail.com>
2024-10-20 20:54:36 -0400

    implemented All In resuable top bar

--- app/src/main/java/com/appdev/all_in_android/MainActivity.kt
+++ app/src/main/java/com/appdev/all_in_android/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.appdev.all_in_android.ui.components.general.AllInTopBar
 import com.appdev.all_in_android.ui.theme.AllinandroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,9 +20,28 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AllinandroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    topBar = { AllInTopBar(title = "Home", money = 1000) },
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
                 }
             }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AllinandroidTheme {
+        Scaffold(
+            topBar = { AllInTopBar(title = "Home", money = 1000) },
+            modifier = Modifier.fillMaxSize()
+        ) { innerPadding ->
+            Text(
+                text = "Hello, World!",
+                modifier = Modifier.padding(innerPadding)
+            )
         }
     }
 }
--- app/src/main/java/com/appdev/all_in_android/ui/components/general/AllInTopBar.kt
+++ app/src/main/java/com/appdev/all_in_android/ui/components/general/AllInTopBar.kt
@@ -1,21 +1,33 @@
 package com.appdev.all_in_android.ui.components.general
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.appdev.all_in_android.R
 
-
+/**
+ * Reusable Top Bar for Scaffold
+ * Takes in title of screen and amount of money user has
+ */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AllInTopBar(
@@ -24,13 +36,42 @@ fun AllInTopBar(
 ) {
     MediumTopAppBar(
         title = {
-            Text(
-                text = title,
-                fontSize = 40.sp,
-                fontWeight = FontWeight.Bold
-            )
-        }
-    )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        end = 16.dp
+                    ),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = title,
+                    fontSize = 36.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_red_money),
+                        contentDescription = null,
+                        tint = Color.Unspecified
+                    )
+                    Text(
+                        text = "$money",
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+        },
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = Color.White,
+        ),
+
+        )
 
 }
 
@@ -39,7 +80,7 @@ fun AllInTopBar(
 private fun AllInTopBarPreview() {
     Scaffold(topBar = {
         AllInTopBar(
-            title = "Profile",
+            title = "Marketplace",
             money = 1000
         )
     }) { padding ->
@@ -48,7 +89,7 @@ private fun AllInTopBarPreview() {
                 .padding(padding)
                 .fillMaxSize()
                 .background(Color.White)
-        ){
+        ) {
 
         }
 

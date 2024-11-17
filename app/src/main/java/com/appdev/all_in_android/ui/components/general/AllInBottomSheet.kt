73adc3331b1a775fdd3ec3d9bdccc0c05a325e63
Andrew Cheung <andrewcheung360@gmail.com>
2024-11-17 15:55:54 -0500

    all in bottom sheet (general)

--- app/src/main/java/com/appdev/all_in_android/ui/components/general/AllInBottomSheet.kt
+++ app/src/main/java/com/appdev/all_in_android/ui/components/general/AllInBottomSheet.kt
@@ -4,10 +4,22 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.SheetState
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.appdev.all_in_android.ui.components.home.RarityChestSheetContent
@@ -62,13 +74,29 @@ data class RarityChestContent(
     val onBuyNow: () -> Unit
 )
 
-//@Preview(showBackground = true)
-//@Composable
-//fun AllInBottomSheetPreview() {
-//    AllInBottomSheet(
-//        showBottomSheetState = true,
-//        sheetState = SheetState(),
-//        onDismiss = {},
-//        bottomSheetType = BottomSheetType.RARITY_CHEST
-//    )
-//}
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun AllInBottomSheetPreview() {
+    var showBottomSheetState by remember { mutableStateOf(false) }
+    val sheetState = rememberModalBottomSheetState()
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Click to show bottom sheet",
+            modifier = Modifier.clickable { showBottomSheetState = true }
+        )
+        if (showBottomSheetState) {
+            AllInBottomSheet(
+                showBottomSheetState = showBottomSheetState,
+                sheetState = sheetState,
+                onDismiss = { showBottomSheetState = false },
+                bottomSheetType = BottomSheetType.RARITY_CHEST,
+                rarityChestContent = RarityChestContent("Common", 100, {})
+            )
+        }
+    }
+}

336afbd604794c80de5af853ad34c6f8be34392c
amjiao <amyjiaowang@gmail.com>
2025-03-23 15:28:27 -0400

    Cart components and HelpScreen setup

---
+++ app/src/main/java/com/appdev/all_in_android/ui/components/cart/CartRowItem.kt
@@ -0,0 +1,52 @@
+package com.appdev.all_in_android.ui.components.cart
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun CartRowItem(
+    title: String,
+    cost: Int,
+    subLine: String
+){
+    Column() {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
+            Text(
+                "${title}",
+                fontSize = 14.sp
+            )
+            Text(
+                "$${cost}"
+            )
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.End
+        ){
+            Text(
+                text = subLine,
+                fontSize = 10.sp,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun CartRowItemPreview(){
+    CartRowItem(
+        "Name 3-Pointer",
+        30,
+        "Win ${80} if true"
+    )
+}
---
+++ app/src/main/java/com/appdev/all_in_android/ui/screens/HelpScreen.kt
@@ -0,0 +1,16 @@
+package com.appdev.all_in_android.ui.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.appdev.all_in_android.ui.theme.backgroundBlue
+
+@Composable
+fun HelpScreen(){
+    Column(
+        modifier = Modifier.background(color = backgroundBlue)
+    ) {
+
+    }
+}

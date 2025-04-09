17e34fece6025b85276709b13f967e7dd25069d1
Caleb Shim <chs232@cornell.edu>
2025-04-09 01:05:48 -0400

    Add design system colors

--- app/src/main/java/com/appdev/all_in_android/ui/theme/Color.kt
+++ app/src/main/java/com/appdev/all_in_android/ui/theme/Color.kt
@@ -20,3 +20,8 @@ val LegendaryYellow = Color(0xFFE9B140)
 
 val GainGreen = Color(0xFF47CD89)
 val CostRed = Color(0xFFF97066)
+
+val Primary = Color(0xFF1F70C7)
+val Secondary = Color(0xFF99C5F4)
+val Background1 = Color(0xFF15141B)
+val Background2 = Color(0xFF201E2D)
